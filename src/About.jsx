521cf1d2bc3b76d3c3017ee4db8f61cfded30b44
David Yoder <david.yoder@live.com>
2024-07-19 20:01:07 -0400

    updated about

--- src/About.jsx
+++ src/About.jsx
@@ -10,15 +10,15 @@ const About = () => {
         className="rounded-full w-32 h-32 object-cover mx-auto mb-4"
       />
       <p className="text-gray-600 mb-4">
-        student @ davidson college with an interest in creation  
+        cs student @ davidson college with an interest in web development and computer graphics (in progress) 
       </p>
       <p className="text-gray-600 mb-4">
-        i enjoy making web apps and editing videos
-      </p>
+        i enjoy making web apps and making visual content
+      </p>  
       <div className="mt-6">
         <h3 className="text-xl font-semibold text-gray-800 mb-2">skills</h3>
         <div className="flex flex-wrap gap-2">
-          {['JavaScript', 'React', 'Node.js', 'Python', 'Git', 'HTML','CSS', 'After Effects', 'Premiere Pro'].map((skill, index) => (
+          {['JavaScript', 'React', 'Node.js', 'Python', 'Git', 'HTML','CSS', 'After Effects', 'Premiere Pro', 'Three.js'].map((skill, index) => (
             <span key={index} className="bg-blue-100 text-blue-800 px-2 py-1 rounded">
               {skill}
             </span>
