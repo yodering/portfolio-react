b3e3d8058760ed51fb12bb565825b5ef30983277
David Yoder <david.yoder@live.com>
2024-07-25 12:14:58 -0400

    updated about.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -9,16 +9,16 @@ const About = () => {
         alt="Yoder" 
         className="rounded-full w-32 h-32 object-cover mx-auto mb-4"
       />
-      <p className="text-gray-600 mb-4">
-        cs student @ davidson college with an interest in web development and computer graphics (in progress) 
+      <p className="text-gray-600 mb-4 text-center">
+        cs student @ davidson college with an interest in web development and computer graphics (in progress).
       </p>
-      <p className="text-gray-600 mb-4">
-        i enjoy making web apps and making visual content
+      <p className="text-gray-600 mb-4 text-center">
+        i enjoy making web apps and creating visual content.
       </p>  
       <div className="mt-6">
-        <h3 className="text-xl font-semibold text-gray-800 mb-2">skills</h3>
-        <div className="flex flex-wrap gap-2">
-          {['JavaScript', 'React', 'Node.js', 'Three.js', 'Python', 'Git', 'HTML','CSS', 'p5.js','After Effects', 'Premiere Pro'].map((skill, index) => (
+        <h3 className="text-xl font-semibold text-gray-800 mb-2 text-center">skills</h3>
+        <div className="flex flex-wrap justify-center gap-2">
+          {['JavaScript', 'React', 'Node.js', 'Three.js', 'Python', 'Git', 'HTML','CSS','p5.js', 'After Effects', 'Premiere Pro', 'Figma', 'Photoshop'].map((skill, index) => (
             <span key={index} className="bg-blue-100 text-blue-800 px-2 py-1 rounded">
               {skill}
             </span>
